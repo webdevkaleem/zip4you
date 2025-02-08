bced378ffe140bb39543b70dd9272943d9f45b78
webdevkaleem <webdevkaleem@gmail.com>
2025-02-08 22:39:48 +0500

    UI: Made the <NoUploads/> smaller in size with less padding

--- src/app/no-uploads.tsx
+++ src/app/no-uploads.tsx
@@ -2,9 +2,9 @@ import { CopyX } from "lucide-react";
 
 export default function NoUploads() {
   return (
-    <div className="mt-20 flex flex-col gap-12 text-center opacity-50">
+    <div className="flex flex-col gap-6 text-center opacity-50">
       <CopyX className="mx-auto h-16 w-16 stroke-1" />
-      <h4>No uploads yet</h4>
+      <h5>No uploads yet</h5>
     </div>
   );
 }
